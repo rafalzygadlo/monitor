f619639e0d9803f08a82b3f23bf0de2789aa48f8
rafal@zygadlo.org <rafal@zygadlo.org>
2025-12-02 20:33:01 +0100

    with simple key

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
+    use Lib\Bootstrap;
+    use Lib\Tools;
     
     ini_set('log_errors', true); // Error/Exception file logging engine.
     ini_set('error_log', 'errors.log'); // Logging file path
     ini_set("display_errors","on");
     error_reporting(E_ALL);
-    
+    session_name("monitor");
+    session_start();
+ 
+
     // autoloader    
     spl_autoload_extensions(".php");
     spl_autoload_register
@@ -22,7 +27,10 @@
     include "Config/db.config.php";
     // end config files
     
-    use Lib\Bootstrap;
+     if (!isset($_SESSION["key"]))
+        $_SESSION["key"] = Tools::RandomString(16);
+    
+    print $_SESSION["key"];
 
     $app = new Bootstrap();
     if(isset($argv))
